d30b4879a74916251ae8b8a4cc3a5bf5245909ea
haijin <life0fun@gmail.com>
2023-02-10 18:54:05 -0800

    using mds layout

--- src/components/wallet/WalletGraphView.js
+++ src/components/wallet/WalletGraphView.js
@@ -8,17 +8,13 @@ import { shortenAddress } from "./Utils";
 var ReactDOM = require("react-dom");
 
 function CreateNode(txn, x, y) {
+  console.log("Creating Node : ", shortenAddress(txn.toAddress));
   return {
     id: txn.toAddress,
     // x: x,
     // y: y,
     size: 80,
-    label:
-      shortenAddress(txn.toAddress) +
-      "\n" +
-      txn.value +
-      " " +
-      txn.assetType,
+    label: shortenAddress(txn.toAddress), // + "\n" + txn.value + " " + txn.assetType,
     data: txn,
   }
 }
@@ -42,104 +38,31 @@ function CreateEdge(txn) {
   }
 }
 
+function GetLayout(currentWallet) {
+  // return {
+  //   type: 'radial', // 'mindmap', // 'dendrogram', // 'indented', // , // Layout type
+  //   focusNode: currentWallet.id,
+  //   center: [600, 400],
+  //   linkDistance: 600,
+  //   nodeSize: 120,
+  //   preventOverlap: true,
+  //   // unitRadius: 100,
+  //   nodeSpacing: 400,
+  //   strictRadial: false,
+  // }
+  return {
+    type: 'mds',
+    center: [600, 400],
+    linkDistance: 200,
+    workerEnabled: true,
+  }
+}
+
 function WalletGraphView(props) {
   const ref = React.useRef(null);
   const [graph, setGraph] = useState(null);
   var width = 1400;
-  var height = 800;
-
-  useEffect(() => {
-    if (!graph) {
-      setGraph(
-        new G6.Graph({
-          container: ReactDOM.findDOMNode(ref.current),
-          width,
-          height,
-          defaultNode: {
-            shape: "node",
-            size: 75,
-            labelCfg: {
-              style: {
-                fontSize: 10,
-                cursor: "pointer",
-              },
-            },
-            style: {
-              stroke: "#B4B4B4",
-              width: 150,
-            },
-          },
-          defaultEdge: {
-            // shape: "polyline",
-            type: "quadratic",
-            style: {
-              endArrow: true,
-              lineWidth: 3,
-            },
-            labelCfg: {
-              autoRotate: true,
-            },
-          },
-          modes: {
-            default: [
-              { type: 'drag-canvas',},
-              { type: 'drag-node', },
-              { type: 'click-select' },
-              { type: 'activate-relations'},
-              {
-                type: "tooltip",
-                formatText(model) {
-                  console.log(" node tooltip model => ", model);
-                  const node_details =  "<strong>Wallet </strong>: " + model.id;
-                  return node_details;
-                },
-              },
-              {
-                type: "edge-tooltip", 
-                formatText(model) {
-                  // The content of tooltip
-                  console.log("edge tooltip model => ", model);
-                  const text =
-                    "<strong>Transaction ID</strong>: " +
-                    model.data.id +
-                    "<br/> <strong>Risk</strong>: " +
-                    model.data.riskTriggered +
-                    " [" +
-                    model.data.risk +
-                    "]" +
-                    "<br/> <strong>Counter Entity</strong>: " +
-                    model.data.counterEntity +
-                    "<br/> <strong>value</strong>: " +
-                    model.data.value +
-                    " " +
-                    model.data.assetType +
-                    "<br/> <strong>Customer</strong>: " +
-                    model.data.customer +
-                    "<br/> <strong>Activity Time</strong>: " +
-                    model.data.activityTime;
-                  // moment.unix(model.data.activityTime).format("YYYY-MM-DD hh:mm:ss");
-                  return text;
-                },
-              },
-            ],
-            layout: {
-              type: 'dendrogram', // 'mindmap', // 'dendrogram', // 'indented', // , // Layout type
-              direction: 'LR', // Layout direction is from the left to the right. Options: 'H' / 'V' / 'LR' / 'RL' / 'TB' / 'BT'
-              // nodeSep: 20, // The distance between nodes
-              // rankSep: 10, // The distance between adjacent levels
-              // radial: true,
-            },
-          },
-        })
-      );
-    }
-  }, []);
-
-  // graph data
-  const graphData = {
-    nodes: [],
-    edges: [],
-  };
+  var height = 1200;
 
   const walletTransactionsMock = [
     {
@@ -323,17 +246,95 @@ function WalletGraphView(props) {
       activityTime: "2018-06-21T11:41:56Z",
     },
   ];
-  // const currentWallet = props.currentWallet;
-  const currentWallet = { id: "0x04786aada9deea2150deab7b3b8911c309f5ed90", currentBalance: 100, assetType: "eth", transactions: walletTransactionsMock};
+  const currentWallet = props.currentWallet;
+  // const currentWallet = { id: "0x04786aada9deea2150deab7b3b8911c309f5ed90", currentBalance: 100, assetType: "eth", transactions: walletTransactionsMock};
   console.log(" WalletGraphView currentWallet => ", currentWallet);
+
+  useEffect(() => {
+    if (!graph) {
+      setGraph(
+        new G6.Graph({
+          container: ReactDOM.findDOMNode(ref.current),
+          width,
+          height,
+          defaultNode: {
+            shape: "node",
+            size: 75,
+            labelCfg: {
+              style: {
+                fontSize: 10,
+                cursor: "pointer",
+              },
+            },
+            style: {
+              stroke: "#B4B4B4",
+              width: 150,
+            },
+          },
+          defaultEdge: {
+            // shape: "polyline",
+            type: "quadratic",
+            style: {
+              endArrow: true,
+              lineWidth: 3,
+            },
+            labelCfg: {
+              autoRotate: true,
+            },
+          },
+          layout: GetLayout(currentWallet),
+          modes: {
+            default: [
+              'drag-canvas', 'drag-node', 'click-select', 'activate-relations', 
+              {
+                type: "tooltip",
+                formatText(model) {
+                  console.log(" node tooltip model => ", model);
+                  const node_details =  "<strong>Wallet </strong>: " + model.id;
+                  return node_details;
+                },
+              },
+              {
+                type: "edge-tooltip", 
+                formatText(model) {
+                  // The content of tooltip
+                  console.log("edge tooltip model => ", model);
+                  const text =
+                    "<strong>Transaction ID</strong>: " +
+                    model.data.id +
+                    "<br/> <strong>Risk</strong>: " +
+                    model.data.riskTriggered +
+                    " [" +
+                    model.data.risk +
+                    "]" +
+                    "<br/> <strong>Counter Entity</strong>: " +
+                    model.data.counterEntity +
+                    "<br/> <strong>value</strong>: " +
+                    model.data.value +
+                    " " +
+                    model.data.assetType +
+                    "<br/> <strong>Customer</strong>: " +
+                    model.data.customer +
+                    "<br/> <strong>Activity Time</strong>: " +
+                    model.data.activityTime;
+                  // moment.unix(model.data.activityTime).format("YYYY-MM-DD hh:mm:ss");
+                  return text;
+                },
+              },
+            ],
+          },
+        })
+      );
+    }
+  }, []);
+
+  // graph data
+  const graphData = {
+    nodes: [],
+    edges: [],
+  };
   if (!_.isEmpty(currentWallet) && !_.isEmpty(currentWallet.transactions)) {
     const transactions = currentWallet.transactions;
-    height = 0;
-    var yCoordGap = 20; // height / transactions.length;
-    const xCoordGap = 30;
-    var yCoord = yCoordGap;
-    var xCoord = xCoordGap;
-
     graphData.nodes.push({
       id: currentWallet.id,
       label:
@@ -342,8 +343,6 @@ function WalletGraphView(props) {
         currentWallet.currentBalance +
         " " +
         currentWallet.assetType, // The label of the node
-      // x: 0,
-      // y: height / 2,
       size: 120,
       labelCfg: {
         style: {
@@ -354,41 +353,19 @@ function WalletGraphView(props) {
 
     var queue = [];
     var processedTxns = [];
-    var existingNodes = [];
-    var processedNodes = [];
-    var multiEdgeNodes = [];
-
     queue.push(currentWallet.id);
-    var levelLen = queue.length;
-
     while (!_.isEmpty(queue)) {
       const headAddr = queue.pop();
-      levelLen--;
-      if (levelLen == 0) {
-        yCoord = 10;   // reset up one breadth level.  
-      }
       for (const txn of transactions) {
         if (
           txn.fromAddress == headAddr &&
           !processedTxns.includes(txn.id) 
-          // && !existingNodes.includes(txn.toAddress)
         ) {
           queue.push(txn.toAddress);
           processedTxns.push(txn.id);
-          existingNodes.push(txn.toAddress);
-
-          yCoord += 100;
-          graphData.nodes.push(CreateNode(txn, xCoord - xCoordGap, yCoord - yCoordGap + 150));
-          if (yCoord > height) {
-            height = yCoord;
-          }
+          graphData.nodes.push(CreateNode(txn));
           graphData.edges.push(CreateEdge(txn));
-          console.log("rendering node x, y", xCoord - xCoordGap, yCoord - yCoordGap + 150, yCoord, height);
         } 
-      }
-      xCoord += 200;
-      if (levelLen == 0) {
-        levelLen = queue.length;
       }
     }
   }
@@ -398,7 +375,7 @@ function WalletGraphView(props) {
     G6.Util.processParallelEdges(graphData.edges, 20);
     graph.changeSize(width, 800);
     graph.render();
-    graph.fitCenter();
+    // graph.fitCenter();
 
     graph.on("node:mouseenter", (event) => {
       console.log("node:mouseenter >>>", event);
